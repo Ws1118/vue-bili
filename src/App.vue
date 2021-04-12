c5f512fba4f0e6c067a53a9a8da0fc149dea4ab1
Ws1118 <2865094521@qq.com>
2021-04-12 17:22:57 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,43 @@
 <template>
- <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app">
+    <i class="iconfont icon-bilibilidonghua"></i>
     <el-row>
-      <el-button>这里应该是bili的icon图标</el-button>
-      <el-button type="primary">主站</el-button>
-      <el-button type="success">番剧</el-button>
-      <el-button type="info">游戏中心</el-button>
-      <el-button type="warning">直播</el-button>
-      <el-button type="danger">会员购</el-button>
-      <el-button type="danger">漫画</el-button>
-      <el-button type="danger">赛事</el-button>
-      <el-button type="danger">下载APP</el-button>
+      <element class="el-button--primary">
+        <!-- <el-button>这里应该是bili的icon图标</el-button> -->
+        <el-button type="primary">主站</el-button>
+        <el-button type="success">番剧</el-button>
+        <el-button type="info">游戏中心</el-button>
+        <el-button type="warning">直播</el-button>
+        <el-button type="danger">会员购</el-button>
+        <el-button type="danger">漫画</el-button>
+        <el-button type="danger">赛事</el-button>
+        <el-button type="danger">下载APP</el-button>
+        <el-button type="info" icon="search"></el-button>
+        <el-button type="danger">大会员</el-button>
+        <el-button type="danger">消息</el-button>
+        <el-button type="danger">动态</el-button>
+        <el-button type="danger">收藏</el-button>
+        <el-button type="danger">历史</el-button>
+        <el-button type="danger">创作中心</el-button>
+        <el-button type="danger">投稿</el-button>
+
+        <!-- <el-button>主站</el-button>
+        <el-button>番剧</el-button>
+        <el-button>游戏中心</el-button>
+        <el-button>直播</el-button>
+        <el-button>会员购</el-button>
+        <el-button>漫画</el-button>
+        <el-button>赛事</el-button>
+        <el-button>下载APP</el-button>
+        <el-button icon="search"></el-button>
+        <el-button>大会员</el-button>
+        <el-button>消息</el-button>
+        <el-button>动态</el-button>
+        <el-button>收藏</el-button>
+        <el-button>历史</el-button>
+        <el-button>创作中心</el-button>
+        <el-button>投稿</el-button> -->
+      </element>
     </el-row>
   </div>
 </template>
@@ -25,7 +52,7 @@
 }
 
 #nav {
-  padding: 30px;
+  padding: 10px;
 
   a {
     font-weight: bold;
@@ -35,5 +62,17 @@
       color: #42b983;
     }
   }
+}
+
+.icon-bilibilidonghua {
+  font-size: 2rem;
+  color: #fff;
+}
+
+/* 更改element-UI按钮样式 */
+.el-button--primary {
+  color: #fff;
+  background-color: #ff69b4;
+  border-color: #ff69b4;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
+import './index.css'
+import "vant/lib/index.css"
+import { Icon } from 'vant'
 
 Vue.config.productionTip = false;
-Vue.use(ElementUI);
+Vue.use(ElementUI).use(Icon);
 
 new Vue({
   router,
